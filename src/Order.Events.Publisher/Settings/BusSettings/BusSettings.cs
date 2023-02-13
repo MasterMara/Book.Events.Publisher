382a13937e8597912882206ff60455cb3e84d39e
Master Mara <mustafa.karacabey.engineerx@gmail.com>
2023-02-13 10:41:31 +0300

    [Added Bus Settings]

---
+++ src/Order.Events.Publisher/Settings/BusSettings/BusSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace Order.Events.Publisher.Settings.BusSettings;
+
+public class BusSettings : IBusSettings
+{
+    public string ClusterName { get; set; }
+    public string[] ClusterMembers { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public int Heartbeat { get; set; }
+    public int PrefetchCount { get; set; }
+    public int ConcurrencyLimit { get; set; }
+    public bool AutoDelete { get; set; }
+    public bool Durable { get; set; }
+}
---
+++ src/Order.Events.Publisher/Settings/BusSettings/IBusSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace Order.Events.Publisher.Settings.BusSettings;
+
+public interface IBusSettings
+{
+    string ClusterName { get; set; }
+    string[] ClusterMembers { get; set; }
+    string Username { get; set; }
+    string Password { get; set; }
+    int Heartbeat { get; set; }
+    int PrefetchCount  { get; set; }
+    int ConcurrencyLimit  { get; set; }
+    bool AutoDelete { get; set; }
+    bool Durable { get; set; }
+}
